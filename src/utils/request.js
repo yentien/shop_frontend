844ca123b0f92e2eeea20465efcdb305e7835b68
Arthur Tien <arhturtien88@gmail.com>
2023-05-02 16:59:21 +0800

    jwt token 過期登出

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,33 +1,47 @@
 import axios from "axios";
-import { useStore } from "vuex";
 import { getJwtToken } from "../apis/auth";
-
-const store = useStore();
+import { router } from "../routes";
+import { store } from "../store";
 
 export async function request(
   url,
   { method = "GET", data, headers, auth = true } = {}
 ) {
-  const res = await axios({
-    method: method,
-    url: url,
-    headers: {
-      "Content-Type": "application/json",
-      // 展開運算符 (spread operator) + 短路運算符 (short-circuit operator) 
-      ...(auth && { Authorization: `Bearer ${getJwtToken()}` }),
-      ...headers,
-    },
-    ...(data && { data: JSON.stringify(data) }),
-  });
+  try {
+    const res = await axios({
+      method: method,
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+        // 展開運算符 (spread operator) + 短路運算符 (short-circuit operator) 
+        ...(auth && { Authorization: `Bearer ${getJwtToken()}` }),
+        ...headers,
+      },
+      ...(data && { data: JSON.stringify(data) }),
+    });
+    return res;
+  } catch (error) {
+
+    // console.log(error.response.status);
+    // 如果status是401，就呼叫logout函式
+    if (error.response.status === 401) {
+      alert("請重新登入!");
+      await store.dispatch("logoutUser");
+      router.push("/login");
+    }
+  }
 
   // 這邊可以對請求錯誤做初步的處裡
   // if (res.status > 400) {
 
   // }
-  // if (res.status == 403) {
+  // if (res.status == 401) {
   //   await store.dispatch("logoutUser");
   // }
-  // console.log(res.data);
-  // console.log("res.data");
-  return res;
+
+  // console.log(res.status)
+  // console.log(res)
+  // 加上status屬性
+
+
 }
